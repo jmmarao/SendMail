8485904b3e0de8c04cda4010ee38ee29ae9c6c80
jmmarao <jmmarao@gmail.com>
2022-10-14 11:35:16 -0300

    Implement layout and enabled activity view binding

--- app/src/main/java/br/edu/ifsp/sendmail/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/sendmail/MainActivity.kt
@@ -2,10 +2,14 @@ package br.edu.ifsp.sendmail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ifsp.sendmail.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
     }
 }
